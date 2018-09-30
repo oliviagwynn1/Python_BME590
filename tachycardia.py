c13387e5ffd62baeaad0c9259fea28f7953403b2
Olivia Gwynn <oliviagwynn@duke.edu>
2018-09-30 14:56:52 -0400

    Commiting tachycardia

--- tachycardia.py
+++ tachycardia.py
@@ -3,8 +3,25 @@ tachycardic = ["tachycardic"]
 import string
 
 def is_tachycardic(candidate):
-	new_candidate = candidate.strip().lower().replace(" ", "")
+	punctuation = str.maketrans("", "", string.punctuation)
+	new_candidate = candidate.strip().lower().replace(" ", "").translate(punctuation)
 	for word in tachycardic:
 		if word == new_candidate:
 			return True
-		return False			
+		else: 
+			index = 0
+			while index < len(new_candidate):
+				letter = new_candidate[index]
+				if letter == candidate[index]:
+					return True
+				elif letter == candidate[index + 1]:
+					new_candidate = new_candidate.insert(index, candidate[index])
+				return False
+				index = index + 1
+			if word == new_candidate:
+				return True
+			return False
+
+
+	
+	
